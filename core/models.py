a5772b1b8460743cf58d9389a21bc32c3aa8814c
bismarkboateng <bismarkb609@gmail.com>
2023-03-15 22:43:13 +0000

    added models file

--- core/models.py
+++ core/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
+import uuid 
 
-# Create your models here.
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(db_index=True, unique=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(db_index=True, max_length=300, unique=True)
+    first_name = models.CharField(max_length=300)
+    last_name = models.CharField(max_length=300)
+    email = models.EmailField(db_index=True, unique=True)
+    profile_pic = models.ImageField(upload_to="assets", default="assets/profile.png")
+    created = models.DateTimeField(auto_now=True)
+    updated = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.email}"
+
+
+    @property
+    def name(self):
+        return f"{self.first_name} {self.last_name}"
--- coreRoot/settings.py
+++ coreRoot/settings.py
@@ -9,7 +9,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-r@$%)ovuk8h&)n723%^x+62!xep26s)wm8tf&!(bg&j_88%ce&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -106,8 +105,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = (BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
