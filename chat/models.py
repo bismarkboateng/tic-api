ded137347c14e051d058d078569e2f73b91c7d4e
bismarkboateng <bismarkb609@gmail.com>
2023-03-23 23:20:32 +0000

    added message model

--- chat/models.py
+++ chat/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from user.models import User
 
-# Create your models here.
+class Message(models.Model):
+    message = models.TextField() 
+    sender = models.ForeignKey(User, related_name="sender", on_delete=models.CASCADE) 
+    receiver = models.ForeignKey(User, related_name="receiver", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.receiver.email}"
+    
